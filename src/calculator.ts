ee5a1c205e421e49d10f0401592c80f6dae78c8b
LEEEAAAAAA <lea.schuerch@gmail.com>
2025-03-21 10:56:57 +0100

    Aufgabe 2.2: Produktivcode manipulieren (zurücksetzung)

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,7 +1,7 @@
 export function add(summandA: number, summandB: number): number {
-  return summandA + summandB + 1;
+  return summandA + summandB;
 }
 
 export function subtract(minuend: number, subtrahend: number): number {
-  return minuend - subtrahend + 1;
+  return minuend - subtrahend;
 }
